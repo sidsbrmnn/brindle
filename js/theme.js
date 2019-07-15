1ed47beb5dec3de0923031ac925966b8e9a53bd6
Siddharth Subramanian <sidsbrmnn@gmail.com>
2019-07-16 00:11:48 +0530

    index.js: code refactoring

--- js/theme.js
+++ js/theme.js
@@ -1,9 +1,12 @@
 const navbarNav = document.getElementById("mainNav");
 const navToggler = document.getElementById("navToggler");
 
-navToggler.addEventListener("click", () => navbarNav.classList.toggle("show"));
+navToggler.addEventListener("click", e => {
+  e.preventDefault();
+  navbarNav.classList.toggle("show");
+});
 
-window.onresize = function() {
+window.onresize = () => {
   if (window.innerWidth < 992 && navbarNav.classList.contains("show"))
     navbarNav.classList.remove("show");
 };
